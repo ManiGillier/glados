869aa0b9cc13bf913243a8d9fa22691c19e5a09f
Mani Gillier <mani.gillier@epitech.eu>
2025-09-18 13:53:43 +0200

    fixup! [Feat] Added IF parsing

--- src/Lisp/Parser/Parser.hs
+++ src/Lisp/Parser/Parser.hs
@@ -21,7 +21,7 @@ parseSExpr (S.List [S.Symbol "lambda", S.List args, sContent])
   = A.Lambda <$> parseLambdaArgs args <*> parseSExpr sContent
 parseSExpr (S.List [S.Symbol "define", S.List (S.Symbol name:args), sContent])
   = A.Define name <$> (A.Lambda <$> parseLambdaArgs args <*> parseSExpr sContent)
-parseSExpr (S.List [S.Symbol "If", cond, true, false])
+parseSExpr (S.List [S.Symbol "if", cond, true, false])
   = A.If <$> (parseSExpr cond) <*> (parseSExpr true) <*> (parseSExpr false)
 parseSExpr (S.List (S.Symbol name:args))
   = A.Call name <$> mapM parseSExpr args

b8100f2d6ae834e3659f5eef2fe85c007a0292c0
Boo-cher <73936449+Boo-cher@users.noreply.github.com>
2025-09-22 10:31:12 +0200

    [Fix] warnings

--- src/Lisp/Lexer/Lexer.hs
+++ src/Lisp/Lexer/Lexer.hs
@@ -5,67 +5,68 @@
 -- Lexer
 --}
 
-module Lisp.Lexer.Lexer ( lexe )
-    where
+module Lisp.Lexer.Lexer (lexe)
+where
 
+import Data.Void (Void)
+import Lisp.DataStruct.SymbolicExpression (SExpr (..))
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Text.Megaparsec.Char.Lexer ( signed, decimal, symbol )
-import Data.Void ( Void )
-import Data.Char(isSpace)
-import Lisp.DataStruct.SymbolicExpression(SExpr(..))
-import Text.Read (readMaybe)
+import Text.Megaparsec.Char.Lexer (decimal, signed, symbol)
 
 type Lexer = Parsec Void String
 
 skipWhitespace :: Lexer ()
 skipWhitespace = space
 
-readSymbol' :: Lexer SExpr
-readSymbol' = do
-    _ <- skipWhitespace
-    sym <- takeWhile1P Nothing (\c -> not (isSpace c) && c /= '(' && c /= ')')
-    _ <- skipWhitespace
-    return $ Symbol sym
+-- readSymbol' :: Lexer SExpr
+-- readSymbol' = do
+--     _ <- skipWhitespace
+--     sym <- takeWhile1P Nothing (\c -> not (isSpace c) && c /= '(' && c /= ')')
+--     _ <- skipWhitespace
+--     return $ Symbol sym
 
 readSymbol :: Lexer SExpr
-readSymbol = Symbol <$>
-  some (noneOf " \t\n()")
-  <* space
+readSymbol =
+    Symbol
+        <$> some (noneOf " \t\n()")
+        <* space
 
-readValue' :: Lexer SExpr
-readValue' = do
-    _ <- skipWhitespace
-    sign <- optional (char '-' <|> char '+')
-    word <- takeWhile1P Nothing (\c -> not (isSpace c) && c /= '(' && c /= ')')
-    _ <- skipWhitespace
-    case readMaybe word of
-        Just num -> return $ case sign of
-            Just '-' -> Value (-num)
-            Just _ -> Value num
-            Nothing -> Value num
-        Nothing -> fail "Not a valid integer"
+-- readValue' :: Lexer SExpr
+-- readValue' = do
+--     _ <- skipWhitespace
+--     sign <- optional (char '-' <|> char '+')
+--     word <- takeWhile1P Nothing (\c -> not (isSpace c) && c /= '(' && c /= ')')
+--     _ <- skipWhitespace
+--     case readMaybe word of
+--         Just num -> return $ case sign of
+--             Just '-' -> Value (-num)
+--             Just _ -> Value num
+--             Nothing -> Value num
+--         Nothing -> fail "Not a valid integer"
 
 readValue :: Lexer SExpr
-readValue = Value <$>
-  signed (return ()) decimal
-  <* notFollowedBy (noneOf " \t\n()")
-  <* skipWhitespace
+readValue =
+    Value
+        <$> signed (return ()) decimal
+        <* notFollowedBy (noneOf " \t\n()")
+        <* skipWhitespace
 
-readSList' :: Lexer SExpr
-readSList' = do
-    _ <- skipWhitespace
-    _ <- char '('
-    list <- many readSExpr
-    _ <- skipWhitespace
-    _ <- char ')'
-    _ <- skipWhitespace
-    return $ List list
+-- readSList' :: Lexer SExpr
+-- readSList' = do
+--     _ <- skipWhitespace
+--     _ <- char '('
+--     list <- many readSExpr
+--     _ <- skipWhitespace
+--     _ <- char ')'
+--     _ <- skipWhitespace
+--     return $ List list
 
 readSList :: Lexer SExpr
-readSList = List <$>
-  between (symbol (return ()) "(") (symbol (return ()) ")") (readManySExpr)
-  <* space
+readSList =
+    List
+        <$> between (symbol (return ()) "(") (symbol (return ()) ")") (readManySExpr)
+        <* space
 
 readSExpr :: Lexer SExpr
 readSExpr = try readSList <|> try readValue <|> readSymbol
--- src/Lisp/Shell/Repl.hs
+++ src/Lisp/Shell/Repl.hs
@@ -6,62 +6,68 @@
 -}
 
 module Lisp.Shell.Repl (repl) where
-import System.IO
-import Control.Monad (unless)
-import Lisp.Lexer.Lexer ( lexe )
-import System.Exit (exitWith, exitSuccess, ExitCode (ExitFailure))
-import Text.Megaparsec (ParseErrorBundle, errorBundlePretty)
+
 import Data.Void (Void)
-import Lisp.DataStruct.SymbolicExpression (SExpr)
-import Lisp.Parser.Parser (parseSExpr)
 import Lisp.DataStruct.Ast (Ast)
-import Lisp.Exec.SymboleTable (SymTable)
+import Lisp.DataStruct.SymbolicExpression (SExpr)
 import Lisp.Exec.Exec (execLisp)
-import Data.Maybe (fromJust)
+import Lisp.Exec.SymboleTable (SymTable)
+import Lisp.Lexer.Lexer (lexe)
+import Lisp.Parser.Parser (parseSExpr)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+import System.IO
+import Text.Megaparsec (ParseErrorBundle, errorBundlePretty)
 
-data Storage = Storage {
-    ibuf :: !String,
-    symTable :: !(SymTable, [String])
-}
+data Storage = Storage
+    { ibuf :: !String
+    , symTable :: !(SymTable, [String])
+    }
 
-repl' :: IO ()
-repl' = do
-    end <- isEOF
-    unless end $ do
-        content <- getLine
-        case content of
-            "quit" -> return()
-            "q" -> return()
-            _ -> putStrLn content >> repl
-            --                  put rayane's func before the content in the paranthesis
+-- repl' :: IO ()
+-- repl' = do
+--     end <- isEOF
+--     unless end $ do
+--         content <- getLine
+--         case content of
+--             "quit" -> return ()
+--             "q" -> return ()
+--             _ -> putStrLn content >> repl
+
+--                  put rayane's func before the content in the paranthesis
 
 repl :: IO ()
 repl = replSingle $ Storage "" ([], [])
 
 execFold :: (SymTable, [String]) -> Ast -> (SymTable, [String])
-execFold (table', str') ast = (table, str:str')
-  where (str, table) = execLisp ast table'
+execFold (table', str') ast = (table, str : str')
+  where
+    (str, table) = execLisp ast table'
 
-manageAfterLexing :: Storage
-  -> Either (ParseErrorBundle String Void) ([SExpr], String)
-  -> IO ()
-manageAfterLexing _ (Left e) = hPutStr stderr (errorBundlePretty e)
-  >> exitWith (ExitFailure 84)
+manageAfterLexing ::
+    Storage ->
+    Either (ParseErrorBundle String Void) ([SExpr], String) ->
+    IO ()
+manageAfterLexing _ (Left e) =
+    hPutStr stderr (errorBundlePretty e)
+        >> exitWith (ExitFailure 84)
 manageAfterLexing s (Right (value, str)) =
-  print value >>
-  print expressions >>
-  print final >>
-  case final of
-       Just (table, result) ->
-         mapM putStrLn (filter (\str' -> not $ null str') result) >>
-         replSingle (s { ibuf = str, symTable = (table, [])} )
-       Nothing -> hPutStrLn stderr "Parsing error." >> exitWith (ExitFailure 84)
-  where expressions = sequence $ map parseSExpr value
-        final = foldl' execFold (symTable s) <$> expressions
+    print value
+        >> print expressions
+        >> print final
+        >> case final of
+            Just (table, result) ->
+                mapM putStrLn (filter (\str' -> not $ null str') result)
+                    >> replSingle (s{ibuf = str, symTable = (table, [])})
+            Nothing -> hPutStrLn stderr "Parsing error." >> exitWith (ExitFailure 84)
+  where
+    expressions = sequence $ map parseSExpr value
+    final = foldl' execFold (symTable s) <$> expressions
 
 replSingle :: Storage -> IO ()
-replSingle s = putStr ("> " ++ ibuf s) >> hFlush stdout >> isEOF >>= \ isEof ->
-  if isEof then
-    return ()
-  else
-    (fmap (\ str' -> lexe (ibuf s ++ str')) getLine) >>= manageAfterLexing s
+replSingle s =
+    putStr ("> " ++ ibuf s) >> hFlush stdout >> isEOF >>= \isEof ->
+        if isEof
+            then
+                return ()
+            else
+                (fmap (\str' -> lexe (ibuf s ++ str')) getLine) >>= manageAfterLexing s

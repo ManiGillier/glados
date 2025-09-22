38a019c1020aeca164fd3c749ad708c9626e043e
Mani Gillier <mani.gillier@epitech.eu>
2025-09-22 10:48:48 +0200

    [Feat] Added some tests + tasks.org

--- test.scm
+++ test.scm
@@ -1,2 +1,16 @@
 (define add  (lambda (a b)    (+ a b)))(add 3 4)
 (define (sub a b) (- a b  )) (sub     3 4)
+
+(define (> a b)
+  (if (eq? a b)
+      #f
+      (if (< a b)
+          #f
+          #t)))
+(> 10 -2)
+
+(define (fact x)
+  (if (eq? x 1)
+      1
+      (* x (fact (- x 1)))))
+(fact 10)

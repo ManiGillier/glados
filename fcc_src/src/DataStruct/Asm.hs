698b57ad6da9179f49d17956ed514b7a5ece9f93
Mani Gillier <mani.gillier@epitech.eu>
2025-09-29 12:13:50 +0200

    [Feat] Added ASM

---
+++ fcc_src/src/DataStruct/Asm.hs
@@ -0,0 +1,35 @@
+{-
+-- EPITECH PROJECT, 2025
+-- glados
+-- File description:
+-- Asm
+-}
+
+module DataStruct.Asm where
+import Data.Binary (Binary)
+
+type LabelName = String
+type VariableName = String
+
+data Value =
+  StackPos !Int
+  | GlobalPos !Int
+  | RelativePos !Int
+  | LabelPos !LabelName
+  | Variable !VariableName
+  deriving (Show)
+
+data Instruction =
+  Label !LabelName
+  | Add
+  | Sub
+  -- Add more here
+  | Push !Value
+  | Pop !Value
+  | Call !LabelName
+  | Ret
+  | Jmp
+  | ZJmp
+  deriving (Show)
+
+type Asm = [Instruction]

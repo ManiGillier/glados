35975805364837b551abb49e7a032bd6b9aa57e3
Mani Gillier <mani.gillier@epitech.eu>
2025-10-01 11:04:38 +0200

    [Fix] United operation and computable management

--- fcc_src/src/Compiler/Operation.hs
+++ fcc_src/src/Compiler/Operation.hs
@@ -5,10 +5,24 @@
 -- compiler of operations
 -}
 
-module Compiler.Operation (compileOperation) where
-
+module Compiler.Operation ( compileOperation
+                          , compileComputable) where
 
 import DataStruct.Ast.Ast as Ast
 import DataStruct.Asm as Asm
 import DataStruct.Ast.Variable (VariableValue(Int, Bool))
-import qualified Compiler.Compute as Compute
+
+compileComputable :: Computable -> Maybe [Instruction]
+-- Computable Value
+compileComputable (Value (Int a)) = Just $ [PushValue a]
+compileComputable (Value (Bool False)) = Just $ [PushValue 0]
+compileComputable (Value (Bool True)) = Just $ [PushValue 1]
+compileComputable (Value _) = Nothing
+-- Computable Variable
+compileComputable (Ast.Variable _) = error $ "TODO: Implement variable"
+-- Computable Operation
+compileComputable (Ast.Operation op) = compileOperation op
+-- compileComputable _ = Nothing
+
+compileOperation :: Operation -> Maybe [Instruction]
+compileOperation _ = Nothing

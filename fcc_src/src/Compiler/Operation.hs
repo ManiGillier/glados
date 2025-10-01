c7682d997335a7c1955a58e197e8fb08853d3f9c
Mani Gillier <mani.gillier@epitech.eu>
2025-10-01 11:25:57 +0200

    [Feat] Added computable compilation into ast + ex

--- fcc_src/src/Compiler/Operation.hs
+++ fcc_src/src/Compiler/Operation.hs
@@ -12,6 +12,32 @@ import DataStruct.Ast.Ast as Ast
 import DataStruct.Asm as Asm
 import DataStruct.Ast.Variable (VariableValue(Int, Bool))
 
+getUnOp :: UnaryOperator -> Instruction
+getUnOp Ast.BinaryNot = Asm.BinNot
+getUnOp Ast.BooleanNot = Asm.BoolNot
+getUnOp Ast.Negate = Asm.Negate
+
+getBinOp :: BinaryOperator -> Instruction
+getBinOp Ast.BinaryAnd = Asm.BinAnd
+getBinOp Ast.BinaryOr = Asm.BinOr
+getBinOp Ast.BooleanAnd = Asm.BoolAnd
+getBinOp Ast.BooleanOr = Asm.BoolOr
+getBinOp Ast.Xor = Asm.Xor
+getBinOp Ast.BitShiftLeft = Asm.BitShiftLeft
+getBinOp Ast.BitShiftRight = Asm.BitShiftRight
+getBinOp Ast.Add = Asm.Add
+getBinOp Ast.Sub = Asm.Sub
+getBinOp Ast.Multiplication = Asm.Mult
+getBinOp Ast.Division = Asm.Div
+getBinOp Ast.Modulo = Asm.Mod
+getBinOp Ast.Superior = Asm.Gt
+getBinOp Ast.SuperiorOrEq = Asm.Ge
+getBinOp Ast.Inferior = Asm.Lt
+getBinOp Ast.InferiorOrEq = Asm.Le
+getBinOp Ast.Equals = Asm.Eq
+getBinOp Ast.Is = Asm.Is
+getBinOp Ast.Different = Asm.Diff
+
 compileComputable :: Computable -> Maybe [Instruction]
 -- Computable Value
 compileComputable (Value (Int a)) = Just $ [PushValue a]
@@ -25,4 +51,9 @@ compileComputable (Ast.Operation op) = compileOperation op
 -- compileComputable _ = Nothing
 
 compileOperation :: Operation -> Maybe [Instruction]
-compileOperation _ = Nothing
+compileOperation (UnaryOperation op computable) =
+  liftA2 (++) (compileComputable computable) (Just [getUnOp op])
+compileOperation (BinaryOperation op c0 c1) =
+  liftA2 (++) values (Just [getBinOp op])
+  where values = liftA2 (++) (compileComputable c0) (compileComputable c1)
+-- compileOperation _ = Nothing

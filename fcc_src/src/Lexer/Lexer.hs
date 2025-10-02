89fedd6f311b23acc1b90711aeb622876bbb4769
KaitoMomota <spam.clash.iyad@gmail.com>
2025-10-02 07:19:06 +0200

    [⚙️] Fixed an issue where you were able to not have any space in between keywords while lexing, and moved syntax into a different file

--- fcc_src/src/Lexer/Lexer.hs
+++ fcc_src/src/Lexer/Lexer.hs
@@ -6,13 +6,17 @@
 -}
 
 module Lexer.Lexer(skipWhitespace, readWord, readValue, lexStrings,
-    lexStringsWithTokens', lexStringsWithTokens, readAssign, readAssign') where
+    lexStringsWithTokens', lexStringsWithTokens, readAssign, readAssign', readCondition) where
 
 import Data.Void (Void)
+
 import DataStruct.Lexing(SExpr(..))
+import Lexer.Syntax(assignNameSyntax, assignValueSyntax, assignSyntax, assignSyntax', ifSyntax)
+
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer (decimal, signed)
+
 
 type Lexer = Parsec Void String
 
@@ -20,7 +24,7 @@ skipWhitespace :: Lexer ()
 skipWhitespace = space
 
 readWord :: Lexer SExpr
-readWord = Symbol <$> some (noneOf " .\t\n") <* space
+readWord = Symbol <$> some (noneOf " .\t\n,") <* skipWhitespace
 
 readValue :: Lexer SExpr
 readValue =
@@ -34,35 +38,36 @@ readEOI = char '.'
 
 lexStrings :: [String] -> a -> Lexer a
 lexStrings [] a = return a
-lexStrings (x:xs) a = skipWhitespace *> string x *> skipWhitespace *>
+lexStrings (x:xs) a = skipWhitespace *> string x *> space1 *> skipWhitespace *>
     lexStrings xs a
 
 lexStringsWithTokens' :: [SExpr] -> [String] -> Lexer [SExpr]
 lexStringsWithTokens' t [] = return t
 lexStringsWithTokens' t ("<S>": xs) = readWord >>= \toAdd ->
     lexStringsWithTokens' (t ++ [toAdd]) xs
+lexStringsWithTokens' t ("<S>,": xs) = readWord >>= \toAdd ->
+    lexStringsWithTokens' (t ++ [toAdd]) xs
 lexStringsWithTokens' t ("<V>": xs) = readValue >>= \toAdd ->
     lexStringsWithTokens' (t ++ [toAdd]) xs
-lexStringsWithTokens' t (x:xs) = skipWhitespace *> string x *>
+lexStringsWithTokens' t [x] = skipWhitespace *> string x
+    *> skipWhitespace *> lexStringsWithTokens' t []
+lexStringsWithTokens' t (x:xs) = skipWhitespace *> string x *> space1 *>
     skipWhitespace *> lexStringsWithTokens' t xs
 
 lexStringsWithTokens :: [String] -> Lexer [SExpr]
 lexStringsWithTokens toLex = lexStringsWithTokens' [] toLex
 
-assignName :: [String]
-assignName = words "J'aimerais que <S>"
-
-assignValue :: [String]
-assignValue = words "prenne la valeur <V>"
-
-assignSentence :: [String]
-assignSentence = words "J'aimerais que <S> prenne la valeur <V>"
-
 readAssign :: Lexer [SExpr]
-readAssign = lexStringsWithTokens' [Assign] assignSentence <* readEOI
+readAssign = try (lexStringsWithTokens' [Assign] assignSyntax <* readEOI) <|>
+    (lexStringsWithTokens' [Assign] assignSyntax' <* readEOI)
+
+readCondition :: Lexer [SExpr]
+readCondition = do
+    m <- lexStringsWithTokens' [If] ifSyntax
+    return m
 
 readAssign' :: Lexer [SExpr]
 readAssign' = (\ws1 ws2 -> Assign : ws1 ++ ws2)
-    <$> lexStringsWithTokens assignName
-    <*> lexStringsWithTokens assignValue
+    <$> lexStringsWithTokens assignNameSyntax
+    <*> lexStringsWithTokens assignValueSyntax
     <* readEOI
---
+++ fcc_src/src/Lexer/Syntax.hs
@@ -0,0 +1,23 @@
+{-
+-- EPITECH PROJECT, 2025
+-- fcc_src [WSL: Ubuntu-24.04]
+-- File description:
+-- Syntax
+-}
+
+module Lexer.Syntax(assignNameSyntax, assignValueSyntax, assignSyntax, assignSyntax', ifSyntax) where
+
+assignNameSyntax :: [String]
+assignNameSyntax = words "J'aimerais que <S>"
+
+assignValueSyntax :: [String]
+assignValueSyntax = words "prenne la valeur <V>"
+
+assignSyntax :: [String]
+assignSyntax = words "J'aimerais que <S> prenne la valeur <V>"
+
+assignSyntax' :: [String]
+assignSyntax' = words "<S> prend la valeur <V>"
+
+ifSyntax :: [String]
+ifSyntax = words "Si <S> , exécute le texte :"

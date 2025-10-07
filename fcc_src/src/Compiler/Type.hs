fa92867f3cb666def00cf14b72a78bdf903b81c6
Mani Gillier <mani.gillier@epitech.eu>
2025-10-07 10:49:14 +0200

    [Feat] Added the parsing of conditions

--- fcc_src/src/Compiler/Type.hs
+++ fcc_src/src/Compiler/Type.hs
@@ -5,7 +5,9 @@
 -- compiler type
 -}
 
-module Compiler.Type (Compiler, combine) where
+module Compiler.Type (Compiler, combine
+                     , prefixCompiler
+                     , suffixCompiler) where
 import Compiler.Variable (VariableStorage)
 import DataStruct.Asm (Instruction)
 
@@ -24,3 +26,13 @@ combine ca cb =
   \s (a,b) -> ca s a >>= (
     \(s',ra') -> cb s' b >>= (
       \(s'',rb) -> Just (s'', (ra' ++ rb))))
+
+prefixCompiler :: [Instruction] -> Compiler a -> Compiler a
+prefixCompiler i c = \ s a -> case c s a of
+  Nothing -> Nothing
+  Just (s', is) -> Just (s', i ++ is)
+
+suffixCompiler :: [Instruction] -> Compiler a -> Compiler a
+suffixCompiler i c = \ s a -> case c s a of
+  Nothing -> Nothing
+  Just (s', is) -> Just (s', is ++ i)

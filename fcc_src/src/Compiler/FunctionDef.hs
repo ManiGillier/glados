37aa4d4d556eaaa5b14bbce5a1fcd68331474f01
Mani Gillier <mani.gillier@epitech.eu>
2025-10-09 16:42:33 +0200

    [Feat] Added compiliation of func def
    (including main)

---
+++ fcc_src/src/Compiler/FunctionDef.hs
@@ -0,0 +1,37 @@
+{-
+-- EPITECH PROJECT, 2025
+-- glados
+-- File description:
+-- compile function def
+-}
+
+module Compiler.FunctionDef ( compileFuncDef
+                            , compileMainDef
+                            ) where
+
+import Compiler.Type (Compiler, Context (functionNames)
+                     , apply, mapCompiler
+                     , (.+), (<@), (@>))
+import DataStruct.Ast.Ast (FunctionDef (..), MainFunctionDef (..))
+import Compiler.VariableDef (compileVarDef)
+import Compiler.FunctionBody (compileFuncBody)
+import DataStruct.Asm (Instruction (..))
+import Compiler.Config (funcLabelPrefix)
+
+compileFuncDef :: Compiler FunctionDef
+compileFuncDef s (Function name _ vs body)
+  | elem name $ functionNames s = Nothing
+  | otherwise = flip apply s' $
+    [Label $ funcLabelPrefix ++ name]
+    <@ (mapCompiler compileVarDef, vs)
+    .+ (compileFuncBody, body)
+    @> [Ret]
+    where s' = s { functionNames = name : functionNames s }
+
+compileMainDef :: Compiler MainFunctionDef
+compileMainDef s (Main vs body) = flip apply s'
+  $ [Label $ funcLabelPrefix ++ "main"]
+  <@ (mapCompiler compileVarDef, vs)
+  .+ (compileFuncBody, body)
+  @> [Ret]
+    where s' = s { functionNames = "main" : functionNames s }

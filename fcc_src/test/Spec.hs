8a247d8ca46582a057dc3c291f7ec8d48a5335a3
Mani Gillier <mani.gillier@epitech.eu>
2025-10-13 13:57:08 +0200

    [Test] Compiler.Variable

--- fcc_src/test/Spec.hs
+++ fcc_src/test/Spec.hs
@@ -1,9 +1,11 @@
 import Test.HUnit
 
 import Tests.Compiler.Type (compilerTest)
+import Tests.Compiler.Variable (variableTest)
 
 tests :: Test
 tests = TestList [ "Compiler" ~: compilerTest
+                 , "Variable" ~: variableTest
                  ]
 
 main :: IO Counts

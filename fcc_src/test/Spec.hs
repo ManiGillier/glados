f5656b0d309b20bc5fd0b164a4a4cddba61f74a4
Mani Gillier <mani.gillier@epitech.eu>
2025-10-14 10:27:08 +0200

    [Test] Compiler.FunctionDef + fix functionBody

--- fcc_src/test/Spec.hs
+++ fcc_src/test/Spec.hs
@@ -4,12 +4,14 @@ import Tests.Compiler.Type (compilerTest)
 import Tests.Compiler.Variable (variableTest)
 import Tests.Compiler.Operation (operationTest)
 import Tests.Compiler.FunctionBody (functionBodyTest)
+import Tests.Compiler.FunctionDef (functionDefTest)
 
 tests :: Test
 tests = TestList [ "Compiler" ~: compilerTest
                  , "Variable" ~: variableTest
                  , "Operations" ~: operationTest
                  , "FunctionBody" ~: functionBodyTest
+                 , "FunctionDef" ~: functionDefTest
                  ]
 
 main :: IO Counts
--- fcc_src/test/Tests/Compiler/FunctionBody.hs
+++ fcc_src/test/Tests/Compiler/FunctionBody.hs
@@ -14,6 +14,13 @@ import DataStruct.Asm as Asm
 import DataStruct.Ast.Ast as Ast
 import DataStruct.Ast.Variable as Var
 import Error.ErrorList (ukVarErr)
+import Data.Int (Int64)
+
+vi :: Int64 -> Ast.Computable
+vi i = Ast.Value $ Var.Int i
+
+vi' :: Int64 -> Instruction
+vi' = Asm.PushValue
 
 v :: Ast.Computable
 v = Ast.Value $ Var.Int 42
@@ -30,6 +37,14 @@ functionBodyTest = TestList
  , "Invoke" ~: compileFuncBody baseContext
    [Invoke "f" [v]]
    ~?= Correct (baseContext, [v',PushLabel "func_f", Call])
+ , "Invoke with args" ~: compileFuncBody baseContext
+   [Invoke "f" [v, vi 41, vi 40]]
+   ~?= Correct (baseContext,
+                [ vi' 40 -- last arg
+                , vi' 41 -- middle arg
+                , v' -- first arg
+                ,PushLabel "func_f"
+                , Call])
  , "if no else" ~: compileFuncBody baseContext
    [If (Condition v) [Return v] Nothing]
    ~?= Correct (Context [] 1 [],
@@ -60,6 +75,13 @@ functionBodyTest = TestList
  , "assign variable" ~: compileFuncBody (Context [("x", (0, 8))] 0 [])
    [Assign "x" v]
    ~?= Correct (Context [("x", (0, 8))] 0 [], [ v', PopToStackPtrRel 0])
+ , "assign multiple variable" ~: compileFuncBody
+   (Context [("x", (0, 8)),("y", (8, 8))] 0 [])
+   [Assign "x" v, Assign "y" (Ast.Value $ Var.Int 41)]
+   ~?= Correct (Context [("x", (0, 8)),("y",(8,8))] 0 [],
+                [ v', PopToStackPtrRel 0
+                , Asm.PushValue 41, PopToStackPtrRel 8
+                ])
  , "assign variable error" ~: compileFuncBody baseContext
    [Assign "x" v]
    ~?= Error ukVarErr "x"

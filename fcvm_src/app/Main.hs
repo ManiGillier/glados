fde58f0181a50cb0e487925e45f56c9a4f445008
Boo-cher <73936449+Boo-cher@users.noreply.github.com>
2025-10-14 17:24:58 +0200

    [Add] progress on main

--- fcvm_src/app/Main.hs
+++ fcvm_src/app/Main.hs
@@ -8,6 +8,26 @@
 module Main (main) where
 
 import Lib
+import FileOpening.FileToBytecode
+import System.IO
+import System.IO.Error
+import System.Environment
+import System.Exit (exitWith, ExitCode(..))
+
+ioErrorReturn :: IOError -> IO ()
+ioErrorReturn _ = exitWith (ExitFailure 84)
+
+truc :: IO ()
+truc = do
+    letruc <- head <$> getArgs
+    withFile letruc ReadMode (hGetContent' fileToByteCode)
 
 main :: IO ()
-main = someFunc
+main = catchIOError truc ioErrorReturn
+
+-- getArgs >>= \args -> withFile (head args) ReadMode fileToByteCode
+
+
+    -- arg <- head <$> getArgs
+    -- if (doesFileExist arg) == return True then fileToByteCode arg
+    -- else return 84

fe4327258773a5481c845d96609f185910697fe6
aymerick <aymerick.soual@epitech.eu>
2025-10-14 15:51:08 +0200

    [Feat] replace name to InstructionToAsm.hs + some details

---
+++ fcc_src/src/Binary/InstructionToAsm.hs
@@ -0,0 +1,64 @@
+{-
+-- EPITECH PROJECT, 2025
+-- glados
+-- File description:
+-- Creation of a readable binary file
+-}
+
+module Binary.InstructionToAsm (readableAsm, readableAsmToString) where
+
+import DataStruct.Asm
+
+translateInstr :: Instruction -> [String]
+translateInstr (Label l)     = [l ++ ":"]
+translateInstr (DataInt i)   = ["    db " ++ show i]
+translateInstr (DataString s)= ["    db " ++ show s]
+translateInstr BinNot        = ["    binnot"]
+translateInstr BoolNot       = ["    boolnot"]
+translateInstr Negate        = ["    negate"]
+translateInstr BinAnd        = ["    binand"]
+translateInstr BinOr         = ["    binor"]
+translateInstr BoolAnd       = ["    booland"]
+translateInstr BoolOr        = ["    boolor"]
+translateInstr Xor           = ["    xor"]
+translateInstr BitShiftLeft  = ["    bitshiftleft"]
+translateInstr BitShiftRight = ["    bitshiftright"]
+translateInstr Add          = ["    add"]
+translateInstr Sub          = ["    sub"]
+translateInstr Mult         = ["    mult"]
+translateInstr Div          = ["    div"]
+translateInstr Mod          = ["    mod"]
+translateInstr Gt           = ["    gt"]
+translateInstr Ge           = ["    ge"]
+translateInstr Lt           = ["    lt"]
+translateInstr Le           = ["    le"]
+translateInstr Eq           = ["    eq"]
+translateInstr Diff         = ["    diff"]
+translateInstr Is           = ["    is"]
+translateInstr UpdateZFlag  = ["    updatezflag"]
+translateInstr (PushValue v)            = ["    pushvalue " ++ show v]
+translateInstr (PushGlobAddr v)         = ["    pushglobaddr " ++ show v]
+translateInstr (PushRelAddr v)          = ["    pushreladdr " ++ show v]
+translateInstr (PushLabel l)            = ["    pushlabel " ++ l]
+translateInstr (PushFromStackPtrRel a)  =
+  ["    pushfromstackptrrel " ++ show a]
+translateInstr (PopToStackPtrRel a)     = ["    poptostackptrrel " ++ show a]
+translateInstr PopEmpty               = ["    popempty"]
+translateInstr (WriteToStackPtrRel a v) =
+  ["    writetostackptrrel " ++ show a ++ " " ++ show v]
+translateInstr Dupl                   = ["    dupl"]
+translateInstr Call                   = ["    call"]
+translateInstr Ret                    = ["    ret"]
+translateInstr Jmp                    = ["    jmp"]
+translateInstr Zjmp                   = ["    zjmp"]
+translateInstr Aff                    = ["    aff"]
+
+listOfStringToString :: [String] -> String
+listOfStringToString [] = ""
+listOfStringToString (x:xs) = x ++ "\n" ++ listOfStringToString xs
+
+readableAsm :: [Instruction] -> [String]
+readableAsm xs = concatMap translateInstr xs
+
+readableAsmToString :: [Instruction] -> String
+readableAsmToString xs = listOfStringToString (concatMap translateInstr xs)

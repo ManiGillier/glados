81862038f0af8a6db2f930a7e024ba5e06cf7c00
Mani Gillier <mani.gillier@epitech.eu>
2025-10-15 15:33:30 +0200

    [Ex] Added example AST

---
+++ fcc_src/test/Examples/Ast.hs
@@ -0,0 +1,63 @@
+{-
+-- EPITECH PROJECT, 2025
+-- glados
+-- File description:
+-- example ast
+-}
+
+module Example.Ast (exampleAst) where
+
+import DataStruct.Ast.Ast as Ast
+import DataStruct.Ast.Type as T
+import DataStruct.Ast.Variable as V
+
+exampleAst :: Ast
+exampleAst = Ast
+  -- MAIN --
+  (Just $ Main
+   -- Main Variables
+   []
+   -- Main Content
+   [ Invoke "foo" [Ast.Value $ V.Int 42, Ast.Value $ V.Int 1]
+   , Invoke "foo"
+     [ Ast.Operation $ Ast.BinaryOperation Ast.Add
+       (Ast.Value $ V.Int 48)
+       (Ast.Value $ V.Int 5)
+     , Ast.Value $ V.Int 2
+     ]
+   , Invoke "bar" [Ast.Value $ V.Int 5]
+   ])
+  -- OTHER FUNCS --
+  [ Ast.Function "foo" V.Void
+    -- FOO Params
+    [ V.FuncParam "a" T.Int, V.FuncParam "b" T.Int ]
+    -- FOO Variables
+    [ VariableDef "c" T.Int $ V.Int 0 ]
+    -- FOO Content
+    [ Assign "c" $ Ast.Operation
+      $ Ast.BinaryOperation
+        Ast.Add
+        (Ast.Variable "a")
+        (Ast.Variable "b")
+    , Assign "c" $ Ast.Operation
+      $ Ast.BinaryOperation
+        Ast.Add
+        (Ast.Variable "c")
+        (Ast.Value $ V.Int 1)
+    , Show $ Ast.Variable "c"
+    ]
+  , Ast.Function "bar" V.Void
+    -- BAR PARAMS
+    [V.FuncParam "a" T.Int]
+    -- BAR VARIABLES
+    [VariableDef "b" T.Int $ V.Int 0]
+    -- BAR BODY
+    [ Assign "b" $ Ast.Value $ V.Int 48
+    , Assign "b" $ Ast.Operation
+      $ Ast.BinaryOperation
+        Ast.Add
+        (Ast.Variable "a")
+        (Ast.Variable "b")
+    , Invoke "foo" [ Ast.Variable "b", Ast.Value $ V.Int (-1) ]
+    ]
+  ]

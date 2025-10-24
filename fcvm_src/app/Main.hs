f5ed001b79d6aea01b6da88094e2edae5f500cb1
Mani Gillier <mani.gillier@epitech.eu>
2025-10-24 15:14:20 +0200

    [Feat] Added error handling in main

--- fcvm_src/app/Main.hs
+++ fcvm_src/app/Main.hs
@@ -7,7 +7,7 @@
 
 module Main (main) where
 
-import FileOpening.FileToBytecode
+import FileOpening.FileToBytecode ( fileToByteCode )
 import System.IO
 import System.IO.Error
 import Prelude
@@ -17,15 +17,24 @@ import System.Exit (exitWith, ExitCode(..))
 import VM.Executor (execFccByteCode, execAllByteCodes, printVMIO)
 import Error.MaybeError
 import Data.Functor ((<&>))
+import Data.Maybe (listToMaybe)
+import System.IO (hPutStrLn, stderr)
 
 ioErrorReturn :: IOError -> IO ()
 ioErrorReturn _ = exitWith (ExitFailure 84)
 
-truc :: IO [Word8]
-truc = fileToByteCode =<< (head <$> getArgs)
+printArgError :: IO ()
+printArgError = hPutStrLn stderr "ERROR: You must provide a file as argument."
+
+getFirstArg :: IO String
+getFirstArg = getArgs >>=
+  (\args -> case listToMaybe args of
+      Nothing -> printArgError >> exitWith (ExitFailure 84)
+      Just arg' -> return arg')
 
 main :: IO ()
-main = truc <&> execFccByteCode <&> execAllByteCodes >>= printVMIO
+main = getFirstArg >>= fileToByteCode <&>
+  execFccByteCode <&> execAllByteCodes >>= printVMIO
 
 -- main :: IO ()
 -- main = catchIOError (printMaybeError putStrLn =<<

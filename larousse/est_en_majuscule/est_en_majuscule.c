06e8c5cb1f65e7b560e5d7885ed488248b09372d
Boo-cher <boo-cher@boo-cher.com>
2025-10-29 18:38:36 +0100

    [Fix] est_en_majuscule

--- larousse/est_en_majuscule/est_en_majuscule.c
+++ larousse/est_en_majuscule/est_en_majuscule.c
@@ -1,6 +1,6 @@
 int est_en_majuscule(int n)
 {
-    if ((n <= 'z') && (n >= 'a'))
+    if ((n <= 'Z') && (n >= 'A'))
         return 1;
     return 0;
 }
--- larousse/est_en_majuscule/test/mainEst_en_majuscule.c
+++ larousse/est_en_majuscule/test/mainEst_en_majuscule.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-    printf("Devrait-être 1 : %i\n", est_en_majuscule('m'));
-    printf("Devrait-être 0 : %i\n", est_en_majuscule('M'));
+    printf("Devrait-être 1 : %i\n", est_en_majuscule('M'));
+    printf("Devrait-être 0 : %i\n", est_en_majuscule('m'));
     printf("Devrait-être 0 : %i\n", est_en_majuscule(12));
     return 0;
 }

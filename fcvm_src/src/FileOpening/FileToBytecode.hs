c597817fb13fab66d059248c6a44288502c4f593
Mani Gillier <mani.gillier@epitech.eu>
2025-10-30 19:24:09 +0100

    [Fix] File opening failure error message
    on fcvm

--- fcvm_src/src/FileOpening/FileToBytecode.hs
+++ fcvm_src/src/FileOpening/FileToBytecode.hs
@@ -9,6 +9,14 @@ module FileOpening.FileToBytecode (fileToByteCode) where
 
 import qualified Data.ByteString as B
 import Data.Word
+import Error.MaybeError (MaybeError (..))
+import Error.ErrorList (fileErr)
+import Control.Exception (try, IOException)
+import Data.Functor ((<&>))
 
-fileToByteCode :: String -> IO [Word8]
-fileToByteCode s = B.unpack <$> B.readFile s
+fileToByteCode :: String -> IO (MaybeError [Word8])
+fileToByteCode s = r <&>
+  \r' -> case r' of
+    Left _ -> Error fileErr s
+    Right x -> Correct x
+  where r = try (B.unpack <$> B.readFile s) :: IO (Either IOException [Word8])

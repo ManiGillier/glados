4a8cc64844e4a0ab538d9d72be7b83dfe6ddee83
KaitoMomota <spam.clash.iyad@gmail.com>
2025-10-30 16:38:51 +0100

    [✨] Finished first version of the [Académie Franc C'Aise]

--- academie-franc-c-aise.py
+++ academie-franc-c-aise.py
@@ -27,10 +27,11 @@ from typing import List
 import sys
 import os
 import subprocess
+import time
 
 ############ Variables constantes #############
 
-testFolderPath = 'fcc_src/larousse'
+testFolderPath = 'larousse'
 vmExecutablePath = "fcvm"
 compilerExecutablePath = "fcc"
 binaryNameFc = "a.fcp"
@@ -94,12 +95,12 @@ def register_valid_folders(subfolders: List[str]):
         if not has_any_file_with_extension_in_folder(testFolder, ".c"):
             print(f"[!] [Académie Franc C'aise] Aucun fichier '.c trouvé dans ' {testFolder.name}, le dossier ne sera donc pas pris en compte.", file=sys.stderr)
             continue
-        if not does_folder_exist(testFolder + "/test"):
+        if not does_folder_exist(str(testFolder) + "/test"):
             print(f"[!] [Académie Franc C'aise] Aucun dossier 'test' trouvé dans {testFolder.name}, le dossier ne sera donc pas pris en compte.", file=sys.stderr)
-        if not has_any_file_with_extension_in_folder(testFolder + "/test", ".fr"):
+        if not has_any_file_with_extension_in_folder(testFolder / "test", ".fr"):
             print(f"[!] [Académie Franc C'aise] Aucun fichier '.fr' trouvé dans '{testFolder.name}/test', le dossier ne sera donc pas pris en compte.", file=sys.stderr)
             continue
-        if not has_any_file_with_extension_in_folder(testFolder + "/test", ".c"):
+        if not has_any_file_with_extension_in_folder(testFolder / "test", ".c"):
             print(f"[!] [Académie Franc C'aise] Aucun fichier '.c trouvé dans ' {testFolder.name}/test, le dossier ne sera donc pas pris en compte.", file=sys.stderr)
             continue
         print(f"[*] [Académie Franc C'aise] Le dossier {testFolder.name} a été enregistré !")
@@ -110,11 +111,6 @@ def can_start_testing() -> bool:
 
     return len(testFolders) > 0
 
-def get_lisp_code_file(folder: str) -> str:
-    return get_any_file_by_extension_in_folder(folder, ".scm")
-
-def get_expected_result_file(folder: str) -> str:
-    return get_any_file_by_extension_in_folder(folder, ".txt")
 
 def is_franc_c_built() -> bool:
     global vmExecutablePath
@@ -122,7 +118,7 @@ def is_franc_c_built() -> bool:
 
     file = Path(vmExecutablePath)
     secondFile = Path(compilerExecutablePath)
-    return file.is_file() and os.access(file, os.X_OK) and secondFile.isfile() and os.access(secondFile, os.X_OK)
+    return file.is_file() and os.access(file, os.X_OK) and secondFile.is_file() and os.access(secondFile, os.X_OK)
 
 def build_franc_c() -> bool:
 
@@ -139,13 +135,25 @@ def read_file(fileName: str) -> str:
 def compile_test_fc(testFolder: str) -> int:
     global compilerExecutablePath
 
-    fcFiles = get_all_files_by_extension_in_folder(testFolder, ".fr") + get_all_files_by_extension_in_folder(testFolder + "/test", ".fr")
+    fcFiles = get_all_files_by_extension_in_folder(testFolder, ".fr") + get_all_files_by_extension_in_folder(testFolder / "test", ".fr")
 
-    p1 = subprocess.Popen(["./" + compilerExecutablePath] + fcFiles, stdout=subprocess.PIPE)
-    p1.stdout.close()
+    if testFolder != Path(testFolderPath + "/afficher_nombre"):
+        fcFiles += get_all_files_by_extension_in_folder(Path(testFolderPath + "/afficher_nombre"), ".fr")
 
-    _, err = p2.communicate()
-    return err.decode("utf-8") == 0 and does_file_exist(binaryNameFc)
+    p1 = subprocess.Popen(["./" + compilerExecutablePath] + fcFiles, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    p1.communicate()
+
+    return p1.returncode == 0 and does_file_exist(binaryNameFc)
+
+def compile_test_c(testFolder: str) -> int:
+    global compilerExecutablePath
+
+    cFiles = get_all_files_by_extension_in_folder(testFolder, ".c") + get_all_files_by_extension_in_folder(testFolder / "test", ".c")
+
+    p1 = subprocess.Popen(["gcc", "-I", str(testFolder / "include")] + cFiles, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    p1.communicate()
+
+    return p1.returncode == 0 and does_file_exist(binaryNameC)
 
 def clean_test_fc() -> None:
     try:
@@ -154,24 +162,70 @@ def clean_test_fc() -> None:
     except Exception as ignore:
         pass
 
-def get_vm_output(testFile: str) -> str:
-    global gladosExecutableName
+def get_franc_c_output() -> str:
+    p1 = subprocess.Popen([f"./{vmExecutablePath}", binaryNameFc], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    p1 = subprocess.Popen(["cat", testFile], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen([f"./{gladosExecutableName}"], stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p1.stdout.close()
+    output, _ = p1.communicate()
+    output = output.decode("utf-8")
+    return output
 
-    output, _ = p2.communicate()
-    output = output.decode("utf-8").replace('> ', '')
+
+def get_c_output() -> str:
+    p1 = subprocess.Popen([f"./{binaryNameC}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output, _ = p1.communicate()
+    output = output.decode("utf-8")
     return output
 
 def test_folder(testFolder: str) -> bool:
 
+    startCompilationFc = time.time()
     if not compile_test_fc(testFolder):
-        print(f"[*] [Académie Franc C'aise] Les dossiers soruces '.fr' n'ont pas pu être compilés {testFolder}, le test est donc raté !")
+        print(f"[*] [Académie Franc C'aise] Les dossiers sources '.fr' n'ont pas pu être compilés, le test est donc raté !")
         return False
+    endCompilationFc = time.time()
+    elapsedTimeFc = endCompilationFc - startCompilationFc
+    print(f"[*] [Académie Franc C'aise] Les sources en Franc C ont pris {endCompilationFc - startCompilationFc:.2f}s à compiler.")
 
-    return True
+    startCompilationC = time.time()
+    if not compile_test_c(testFolder):
+        print(f"[*] [Académie Franc C'aise] Les dossiers sources '.c' n'ont pas pu être compilés, le test est donc raté !")
+        return False
+    endCompilationC = time.time()
+    print(f"[*] [Académie Franc C'aise] Les sources en C ont pris {endCompilationC - startCompilationC:.2f}s à compiler.")
+
+    fastestLanguage = "C" if endCompilationC - startCompilationC <= startCompilationFc - startCompilationFc else "Franc C"
+    print(f"[*] [Académie Franc C'aise] Le langage {fastestLanguage} a été plus rapide à la compilation de {abs((endCompilationFc - startCompilationFc) - (endCompilationC - startCompilationC)):.2f}s\n")
+
+    startExecutionFc = time.time()
+    outputFc = get_franc_c_output()
+    endExecutionFc = time.time()
+    print(f"[*] [Académie Franc C'aise] Les sources en Franc C ont pris {endExecutionFc - startExecutionFc:.3f}s à exécuter.")
+
+    startExecutionC = time.time()
+    outputC = get_c_output()
+    endExecutionC = time.time()
+    print(f"[*] [Académie Franc C'aise] Les sources en C ont pris {endExecutionC - startExecutionC:.3f}s à exécuter.")
+
+
+    fastestLanguage = "C" if endExecutionC - startExecutionC <= endExecutionFc - startExecutionFc else "Franc C"
+    print(f"[*] [Académie Franc C'aise] Le langage {fastestLanguage} a été plus rapide à l'exécution de {abs((endExecutionC - startExecutionC) - (endExecutionFc - startExecutionFc)):.3f}s\n")
+
+    if outputFc != outputC:
+        print(f"[*] [Académie Franc C'aise] Le programme C et Franc C n'ont pas produit le même résultat.\n")
+
+        print("[*] [Académie Franc C'aise] Franc C:")
+        print(outputFc)
+        print()
+        print("[*] [Académie Franc C'aise] C:")
+        print(outputC)
+
+    return outputC == outputFc
+
+def is_tool(name):
+    from shutil import which
+
+    return which(name) is not None
 
 ##########################################
 
@@ -194,20 +248,31 @@ def start_academie_franc_c_aise():
     if not does_folder_exist(testFolderPath):
         print(f"[!] [Académie Franc C'aise] Nous n'avons pas pu trouver le dossier à tester '{testFolderPath}', le programme va donc s'arrêter.")
         exit(84)
+    if not is_tool("gcc"):
+        print("[*] [Académie Franc C'aise] Afin d'utiliser ce programme, il vous faut avoir installer GCC, le programme va donc s'arrêter.\n")
+    print(f"\n[*] [Académie Franc C'aise] Nous récupérons la liste des dossiers à tester..")
     register_valid_folders(get_subfolders(testFolderPath))
     print(f"\n[*] [Académie Franc C'aise] Nous avons pu trouver {len(testFolders)} dossiers à tester !")
     if not can_start_testing():
         print(f"[!] [Académie Franc C'aise] Le programme va donc s'arrêter puisqu''il n'y a aucun dossier à tester.")
+        exit(84)
+    print(testFolders)
+    if not Path(testFolderPath + "/afficher_nombre") in testFolders:
+        print(f"[!] [Académie Franc C'aise] Fonction triviale 'afficher_nombre' non trouvée, le programme va donc s'arrêter..")
         exit(84)
     print("\n[*] [Académie Franc C'aise] Nous sommes prêts à lancer les tests !..\n")
 
     passedTest = 0
 
     for folderToTest in testFolders:
-        print(f"[*] [Académie Franc C'aise] Nous testons {folderToTest.name}..")
-        if (test_folder(folderToTest)):
+        print(f"[*] [Académie Franc C'aise] Nous testons {folderToTest.name}..\n")
+        if test_folder(folderToTest):
             passedTest += 1
+            print(f"[*] [Académie Franc C'aise] Le test {folderToTest.name} est passé !\n")
+        else:
+            print(f"[!] [Académie Franc C'aise] Le test {folderToTest.name} n'est pas passé..\n")
         clean_test_fc()
+        print("\n")
     print(f"\n[*] [Académie Franc C'aise] ({passedTest}/{len(testFolders)}) tests passés !")
     if passedTest != len(testFolders):
         exit(84)
